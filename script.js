3ea34534393485a9d51dec9b230f01f43f897ef7
Manonthemon <klimowicz.maciej@gmail.com>
2023-05-24 20:33:32 +0100

    Started working on highlighting function

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ highlight.classList.add('highlight');
 document.body.append(highlight);
 
 function highlightLink() {
-    console.log('Highlight!')
+  const linkCoords = this.getBoundingClientRect();
+highlight.style.width = `${linkCoords.width}px`;
+highlight.style.height = `${linkCoords.height}px`;
 }
 
 triggers.forEach(a => a.addEventListener('mouseover', highlightLink));
